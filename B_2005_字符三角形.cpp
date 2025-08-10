98ec3f01888571b593b765f49203cbfa033607fd
kiritor <zhoucaiyu18@gmail.com>
2025-08-10 16:53:38 +0800

    daily test 8/10

--- B_2005_字符三角形.cpp
+++ B_2005_字符三角形.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 
 int main() {
-    int n;
+    string n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << "*";
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if(i == 1 && j == 3) {
+            cout << n;
+            }
+
+            if(i == 2 && j != 1 && j != 5) {
+                cout << n;
+            }
+
+            if(i == 3) {
+                cout << n;
+            }
+            
         }
+
         cout << endl;
     }
     return 0;
